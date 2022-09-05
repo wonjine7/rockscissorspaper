e2888779facd87aaaa7ab517be86a1f51ce5c27b
wonjin <wonjine7@naver.com>
2022-09-05 14:14:36 +0900

    아이템 선택하기

--- rockscissorpaper/src/App.js
+++ rockscissorpaper/src/App.js
@@ -1,3 +1,4 @@
+import logo from "./logo.svg";
 import "./App.css";
 import Box from "./component/Box";
 
@@ -8,11 +9,38 @@ import Box from "./component/Box";
 // 5. 3 4 번의 결과를 가지고 누가 이겼는지 승패를 따진다
 // 6. 승패결과에 따라 테두리 색이 바뀐다 (이기면 초록, 지면 빨강, 비기면 검정)
 
+/** 사진을 저장할 수 있는 저장아이템 */
+const choice = {
+  rock: {
+    name: "Rock",
+    img: "https://nationaltoday.com/wp-content/uploads/2021/08/National-Pet-Rock-Day-640x514.jpg",
+  },
+  scissors: {
+    name: "Scissors",
+    img: "https://www.ikea.com/kr/ko/images/products/sy-scissors__0112301_pe263788_s5.jpg?f=s",
+  },
+  paper: {
+    name: "Paper",
+    img: "https://www.collinsdictionary.com/images/full/paper_111691001.jpg",
+  },
+};
+
 function App() {
+  const play = (userChoice) => {
+    console.log("선택됨!", userChoice);
+  };
+
   return (
-    <div className="main">
-      <Box title="You" />
-      <Box title="Computer" />
+    <div>
+      <div className="main">
+        <Box title="You" />
+        <Box title="Computer" />
+      </div>
+      <div className="main">
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
+      </div>
     </div>
   );
 }
